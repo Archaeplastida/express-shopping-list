2ee88b12984ac109aacd6b791c3354e0a50318ca
Saif <saifisami2005@gmail.com>
2024-09-27 14:43:16 -0400

    Added error handling.

--- itemRoutes.js
+++ itemRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express"), router = new express.Router(), shoppingList = require("./fakeDb");
+const express = require("express"), router = new express.Router(), shoppingList = require("./fakeDb"), ExpressError = require("./expressError");
 
-const checkExistence = (objList, name) => {
+const checkExistence = (objList = Array, name = String) => {
     let idx = 0;
-    for (let item of objList){
-        if(item["name"] === name) return [item, idx];
+    for (let item of objList) {
+        if (item["name"] === name) return [item, idx];
         idx++;
     }
     return false;
@@ -14,28 +14,61 @@ router.get("/", (req, res, next) => {
 })
 
 router.get("/:name", (req, res, next) => {
-    const theItem = checkExistence(shoppingList, req.params.name)[0];
-    if (theItem) return res.json(theItem);
-    return res.send("Literally nothing in here.");
+    try {
+        const theItem = checkExistence(shoppingList, req.params.name)[0];
+        if (theItem) return res.json(theItem);
+        throw new ExpressError(req.params.name + " does not exist.", 404);
+    }
+    catch (err) {
+        return next(err);
+    }
 })
 
 router.post("/", (req, res, next) => {
-    console.log(req.body)
-    const newItem = {name: req.body.name, price: req.body.price};
-    shoppingList.push(newItem);
-    res.status(201).json({added:newItem});
+    try {
+        const newItem = { name: req.body.name, price: req.body.price };
+        if (newItem.name && newItem.price) {
+            shoppingList.push(newItem);
+            return res.status(201).json({ added: newItem });
+        }
+        throw new ExpressError("Missing or incorrect parameter(s)!", 406);
+    }
+    catch (err) {
+        return next(err);
+    }
 })
 
 router.patch("/:name", (req, res, next) => {
-    const edittingItem = checkExistence(shoppingList, req.params.name)[1];
-    shoppingList[edittingItem] = {name: req.body.name, price: req.body.price};
-    res.status(201).json({updated:shoppingList[edittingItem]});
+    try {
+        const edittingItem = checkExistence(shoppingList, req.params.name)[1];
+        console.log(edittingItem);
+        if (typeof edittingItem === "number") {
+            if (req.body.name && req.body.price) {
+                shoppingList[edittingItem] = { name: req.body.name, price: req.body.price };
+                return res.status(201).json({ updated: shoppingList[edittingItem] });
+            }
+            throw new ExpressError("Missing or incorrect parameter(s)!", 406);
+        }
+        throw new ExpressError(req.params.name + " does not exist, or you may have not put a 'name' parameter!", 404);
+    }
+    catch (err) {
+        return next(err);
+    }
 })
 
 router.delete("/:name", (req, res, next) => {
-    const itemToDelete = checkExistence(shoppingList, req.params.name)[1];
-    shoppingList.splice(itemToDelete, 1);
-    res.status(201).json({message:"Deleted"});
+    try {
+        const itemToDelete = checkExistence(shoppingList, req.params.name)[1];
+        if (typeof itemToDelete === "number") {
+            shoppingList.splice(itemToDelete, 1);
+            res.status(201).json({ message: "Deleted" });
+        }
+        throw new ExpressError(req.params.name + " does not exist.", 404);
+    }
+    catch (err) {
+        return next(err);
+    }
+
 })
 
 module.exports = router;
